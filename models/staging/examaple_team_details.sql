cec0365d5ccaf4354d1bdb7ef36bcec8d70e2b45
BaoCaiH <caihongbao280996@gmail.com>
2022-06-19 12:25:37 +0200

    docs block

--- models/staging/examaple_team_details.sql
+++ models/staging/examaple_team_details.sql
@@ -2,4 +2,4 @@ WITH team_locations AS (
     SELECT * FROM {{ ref("team_locations") }}
 )
 
-SELECT * FROM team_locations
+SELECT *, name = '{{ var("current_champion") }}' AS is_champion FROM team_locations
